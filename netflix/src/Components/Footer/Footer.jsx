a7f14e535fe7e9c5ce3a4866de6025e4ebf6fe27
Abdulwasiee <wasungncom@gmail.com>
2024-07-06 11:54:41 +0300

    Footer commited

--- netflix/src/Components/Footer/Footer.jsx
+++ netflix/src/Components/Footer/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="outerFooterConatainr">
+        <div className="innerFooterConatainer">
+          <ul className="iconsContainer">
+            <li>
+              <FacebookIcon />
+            </li>
+            <li>
+              <InstagramIcon />
+            </li>
+            <li>
+              <YouTubeIcon />
+            </li>
+          </ul>
+          <div className="footerData">
+            <ul className="container">
+              <li>Audio Descrition</li>
+              <li>Investor Relation</li>
+              <li> Lega Notice</li>
+            </ul>
+            <ul className="container">
+              <li>Help Center</li>
+              <li> Jobs</li>
+              <li> Cookie Preference</li>
+            </ul>
+            <ul className="container">
+              <li>Gift Card</li>
+              <li> Terms Of Use</li>
+              <li> Corporate Information</li>
+            </ul>
+            <ul className="conatiner">
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li> Contact Us</li>
+            </ul>
+          </div>
+          <ul className="serviceCode">
+            <li className="service">service code</li>
+            <li>&#169; 1997-2024 Netflix,Inc.</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- netflix/src/Pages/Home/Home.jsx
+++ netflix/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
 
 function Home() {
   return (
     <>
      <Header/> 
+     <Footer/>
     </>
   )
 }
