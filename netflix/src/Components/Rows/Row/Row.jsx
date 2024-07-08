3e72bee8c23f05a506c25bf86c0d524c034200b9
Abdulwasiee <wasungncom@gmail.com>
2024-07-08 06:35:25 +0300

    trailer handeled

--- netflix/src/Components/Rows/Row/Row.jsx
+++ netflix/src/Components/Rows/Row/Row.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../../../utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
 function Row(props) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
@@ -18,6 +21,27 @@ function Row(props) {
     fetchData();
   }, [props.fetchUrl]);
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.error("Error finding trailer:", error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="row">
       <h1>{props.title}</h1>
@@ -25,6 +49,7 @@ function Row(props) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             src={`${base_url}${
               props.isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -33,6 +58,7 @@ function Row(props) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
