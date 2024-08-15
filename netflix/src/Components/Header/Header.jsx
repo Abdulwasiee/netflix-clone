f5f152c1ae3dfa25a2bdb916d15720402a28b2a8
Abdulwasiee <wasungncom@gmail.com>
2024-08-15 17:48:44 +0300

    bar icon added to drop down menus for small screen and also responsiveness insured

--- netflix/src/Components/Header/Header.jsx
+++ netflix/src/Components/Header/Header.jsx
@@ -1,14 +1,29 @@
 import React, { useEffect } from "react";
-import $ from "jquery"; 
+import $ from "jquery";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import MenuIcon from "@mui/icons-material/Menu";
 import netflixLogo from "../../assets/image/netflixLogo.jpg";
 
 function Header() {
   useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        $(".leftMenu").hide();
+        $(".rightMenu").hide();
+        $(".menu-toggle").show();
+      } else {
+        $(".leftMenu").show();
+        $(".rightMenu").show();
+        $(".menu-toggle").hide();
+        $(".dropdown-menu").removeClass("show"); 
+      }
+    };
+
+    // Handle scroll event for transparent background
     $(window).on("scroll", function () {
       if ($(this).scrollTop() > 0) {
         $("#outerHeadContainer").addClass("transparentBg");
@@ -17,18 +32,32 @@ function Header() {
       }
     });
 
+    // Handle menu toggle
+    $(".menu-toggle").on("click", function () {
+      $(".dropdown-menu").toggleClass("show");
+    });
+
+    // Handle window resize
+    $(window).on("resize", handleResize);
+
+    // Initial check
+    handleResize();
+
     return () => {
+      // Cleanup event listeners on component unmount
       $(window).off("scroll");
+      $(window).off("resize", handleResize);
+      $(".menu-toggle").off("click");
     };
   }, []);
 
   return (
     <div className="outerHeadContainer" id="outerHeadContainer">
       <div className="innerHeadContainer">
+        <div className="headerLogo">
+          <img src={netflixLogo} alt="Netflix Logo" />
+        </div>
         <ul className="leftMenu">
-          <li className="headerLogo">
-            <img src={netflixLogo} alt="Netflix Logo" />
-          </li>
           <li>Home</li>
           <li>Tv Shows</li>
           <li>Movies</li>
@@ -48,6 +77,16 @@ function Header() {
           <li>
             <ArrowDropDownIcon />
           </li>
+        </ul>
+        <div className="menu-toggle">
+          <MenuIcon />
+        </div>
+        <ul className="dropdown-menu">
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+          <li>MyList</li>
         </ul>
       </div>
     </div>
